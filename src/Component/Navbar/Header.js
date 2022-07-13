9401814f9290b9af38bee200a49f39b675ee507c
AhshanHabib26 <habib.net27@gmail.com>
2022-07-13 13:27:22 +0600

    Header Added

---
+++ src/Component/Navbar/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="success" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to='/'>Fake Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to='/' >Home</Nav.Link>
+              <Nav.Link as={Link} to='/about' >About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
