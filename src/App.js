132cfc5558f89046bd0210f2296f3268c06269e2
AhshanHabib26 <habib.net27@gmail.com>
2022-07-13 22:05:54 +0600

    Cart Button Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Component/Navbar/Header';
 import {Routes, Route} from 'react-router-dom'
 import Home from './Component/Home/Home';
 import DetailsPage from './Component/Products/DetailsPage';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/home' element={<Home/>}/>
         <Route path='/products/:id' element={<DetailsPage/>}/>
       </Routes>
+      <ToastContainer/>
     </div>
   );
 }
--- src/Component/Products/DetailsPage.js
+++ src/Component/Products/DetailsPage.js
@@ -1,13 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Styles from '../Styles/DetailsPage.module.css'
+import Styles from "../Styles/DetailsPage.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle, faMinusCircle } from "@fortawesome/free-solid-svg-icons";
+import { toast } from "react-toastify";
 
 const DetailsPage = () => {
   const { id } = useParams();
   const [items, setItems] = useState([]);
+  const [count, setCount] = useState(1);
   const { image, title, category, price, rating, description } = items;
+
+  const handaleIncrement = () => {
+    setCount(count + 1);
+  };
+  const handaleDecrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+      toast.warning("You Have Added At Least One Products");
+    }
+  };
 
   useEffect(() => {
     const url = `https://fakestoreapi.com/products/${id}`;
@@ -24,25 +36,31 @@ const DetailsPage = () => {
         </div>
         <div className="text_details">
           <h1 className={Styles.itemsHeading}>{title}</h1>
-          <div className=" d-flex justify-content-between">
-            <span className=" text-success">
-              Rate: {rating?.rate}
-            </span>
-            <span className=" text-danger ">
-              Count: {rating?.count}
-            </span>
+          <div className=" my-3 d-flex justify-content-between">
+            <span className=" text-success">Rate: {rating?.rate}</span>
+            <span className=" text-danger ">Count: {rating?.count}</span>
           </div>
-          <h1 className=" text-success my-3">$ {price}</h1>
+          <h2 className=" text-success my-3">$ {price}</h2>
           <p className={Styles.itemsDes}>{description}</p>
-          <h5>Category: <span className={Styles.itemsCategory}>{category}</span></h5>
+          <h5>
+            Category: <span className={Styles.itemsCategory}>{category}</span>
+          </h5>
           <div className="cart_area mt-5 d-flex align-items-center justify-content-between">
             <div className="count_num d-flex align-items-center">
-            <FontAwesomeIcon size="xl" icon={faPlusCircle} />
-            <h2 className=" mx-4">0</h2>
-            <FontAwesomeIcon size="xl" icon={faMinusCircle} />
+              <FontAwesomeIcon
+                onClick={handaleIncrement}
+                size="xl"
+                icon={faPlusCircle}
+              />
+              <h3 className=" mx-4">{count}</h3>
+              <FontAwesomeIcon
+                onClick={handaleDecrement}
+                size="xl"
+                icon={faMinusCircle}
+              />
             </div>
-            <div className="cart_btn">
-                <button>Add To Cart</button>
+            <div className={Styles.cartBtn}>
+              <button>Add To Cart</button>
             </div>
           </div>
         </div>
